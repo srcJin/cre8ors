a3b32c3760593d0cf4daba1d04ccae77bbc07608
Kiwhan Song <kwsong0113@gmail.com>
2023-11-30 18:31:35 -0500

    add drag and drop functionality

--- client/components/MindMap/FloatingCardList.vue
+++ client/components/MindMap/FloatingCardList.vue
@@ -14,17 +14,33 @@ const cards: Card[] = [
   { type: "note", title: "Note", text: "This is a note" },
   { type: "url", title: "URL", text: "https://www.google.com" },
 ];
+
+const onDragStart = (event: DragEvent, card: Card) => {
+  if (event.dataTransfer) {
+    const cardData = JSON.stringify(card);
+    event.dataTransfer.setData("application/json", cardData);
+    event.dataTransfer.effectAllowed = "move";
+  }
+};
 </script>
 
 <template>
   <div class="overlay">
-    <input type="text" placeholder="Search..." className="input input-bordered w-full max-w-xs mx-4 mb-4" />
-    <div class="overflow-x-auto">
-      <div class="flex whitespace-no-wrap w-full h-[200px] px-4 gap-4">
-        <template v-for="(card, index) in cards" :key="index">
-          <component v-if="card.type === 'note'" :is="NoteCard" :title="card.title" :text="card.text" />
-          <component v-else :is="UrlCard" :title="card.title" :text="card.text" />
-        </template>
+    <div class="flex flex-row justify-between">
+      <input type="text" placeholder="Search..." className="input input-bordered w-full max-w-xs mx-4 mb-4" />
+      <p class="font-semibold text-info pt-3">Drag and drop to add cards to the mind map.</p>
+      <div class="w-80"></div>
+    </div>
+    <div class="nodes">
+      <div class="overflow-x-auto">
+        <div class="flex whitespace-no-wrap w-full h-[200px] px-4 gap-4">
+          <template v-for="(card, index) in cards" :key="index">
+            <div :draggable="true" @dragstart="onDragStart($event, card)">
+              <component v-if="card.type === 'note'" :is="NoteCard" :title="card.title" :text="card.text" />
+              <component v-else :is="UrlCard" :title="card.title" :text="card.text" />
+            </div>
+          </template>
+        </div>
       </div>
     </div>
   </div>
--- client/views/MindMapView.vue
+++ client/views/MindMapView.vue
@@ -2,11 +2,17 @@
 import { Background } from "@vue-flow/background";
 import { Controls } from "@vue-flow/controls";
 import { VueFlow, useVueFlow } from "@vue-flow/core";
+import { nextTick, watch } from "vue";
 import FloatingCardList from "../components/MindMap/FloatingCardList.vue";
 import NoteCardNode from "../components/MindMap/NoteCardNode.vue";
 import UrlCardNode from "../components/MindMap/UrlCardNode.vue";
 
-useVueFlow({
+let id = 0;
+const getId = () => {
+  return `dndnode_${id++}`;
+};
+
+const { findNode, addNodes, project, vueFlowRef } = useVueFlow({
   nodes: [
     {
       id: "1",
@@ -28,11 +34,61 @@ useVueFlow({
     },
   ],
 });
+
+const onDragOver = (event: DragEvent) => {
+  event.preventDefault();
+
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = "move";
+  }
+};
+
+const onDrop = async (event: DragEvent) => {
+  const cardData = event.dataTransfer?.getData("application/json");
+  const card = JSON.parse(cardData || "");
+
+  const { left, top } = vueFlowRef.value?.getBoundingClientRect() ?? { left: 0, top: 0 };
+
+  const position = project({
+    x: event.clientX - left,
+    y: event.clientY - top,
+  });
+
+  const newNode = {
+    id: getId(),
+    type: card.type,
+    position,
+    label: `${card.type} node`,
+    data: {
+      title: card.title,
+      text: card.text,
+    },
+  };
+
+  addNodes([newNode]);
+
+  // Align node position after drop, so it's centered to the mouse
+  await nextTick(() => {
+    const node = findNode(newNode.id);
+    if (!node) return;
+    const stop = watch(
+      () => node.dimensions,
+      (dimensions) => {
+        if (dimensions.width > 0 && dimensions.height > 0) {
+          node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 };
+          stop();
+        }
+      },
+      { deep: true, flush: "post" },
+    );
+  });
+};
 </script>
 
 <template>
-  <main class="h-screen w-screen pt-[68px] -mt-[68px]">
-    <VueFlow fit-view-on-init :auto-connect="true" :default-zoom="1.5" :min-zoom="0.2" :max-zoom="4">
+  <main class="dndflow h-screen w-screen pt-[68px] -mt-[68px]" @drop="onDrop">
+    <!-- @vue-expect-error -->
+    <VueFlow fit-view-on-init :auto-connect="true" :default-zoom="1.5" :min-zoom="0.2" :max-zoom="4" @dragover="onDragOver">
       <template #node-note="noteNodeProps">
         <NoteCardNode v-bind="noteNodeProps" />
       </template>
@@ -42,8 +98,8 @@ useVueFlow({
       <Background pattern-color="#aaa" :gap="8" />
       <Controls />
       <button class="btn btn-primary absolute top-0 right-0 m-[15px] z-50">Save</button>
+      <FloatingCardList />
     </VueFlow>
-    <FloatingCardList />
   </main>
 </template>
 
