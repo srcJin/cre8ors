947c19ccbd7f3329d6b6062af827a6246363dc55
Jin G <gaojin@mit.edu>
2023-11-30 21:24:20 -0500

    developing

--- client/components/MindMap/FloatingCardList.vue
+++ client/components/MindMap/FloatingCardList.vue
@@ -44,7 +44,7 @@ async function getCards(author?: string) {
   let i = 0;
   for (i = 0; i < cardResults.length; i++) {
     console.log("cardResults[i]=", i, cardResults[i].content);
-    cards.value[i] = { type: "note", title: cardResults[i].author, text: cardResults[i].content };
+    cards.value[i] = { type: "note", _id: cardResults[i]._id, title: cardResults[i].author, text: cardResults[i].content };
   }
   console.log("cards=", cards);
 }
@@ -73,8 +73,8 @@ onBeforeMount(async () => {
     <div class="nodes">
       <div class="overflow-x-auto">
         <div class="flex whitespace-no-wrap w-full h-[200px] px-4 gap-4">
-          <!-- <template v-for="(card, index) in cards" :key="index"> -->
-          <div v-for="card in cards" :key="card._id">
+          <div v-for="(card, index) in cards" :key="index">
+            <!-- <div v-for="card in cards" :key="card._id"> -->
             <div :draggable="true" @dragstart="onDragStart($event, card)">
               <!-- <component v-if="card.type === 'note'" :is="NoteCard" :title="card.title" :text="card.text" />
               <component v-else :is="UrlCard" :title="card.title" :text="card.text" /> -->
--- client/components/MindMap/types.ts
+++ client/components/MindMap/types.ts
@@ -2,6 +2,7 @@ import type { Node } from "@vue-flow/core";
 
 export type CustomNodeTypes = "note" | "url";
 export interface CustomData {
+  _id: string;
   title: string;
   text: string;
 }
