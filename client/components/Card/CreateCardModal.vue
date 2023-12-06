7c25166b8fee1208e4dac2b8d0689d565017ebf8
Kiwhan Song <kwsong0113@gmail.com>
2023-12-05 21:43:26 -0500

    add card type, implement new reusable card component

---
+++ client/components/Card/CreateCardModal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useCardStore } from "../../stores/card";
+import { CardType } from "../../types/card";
+
+const title = ref("");
+const type = ref<CardType>(CardType.note);
+const content = ref("");
+const { createCard } = useCardStore();
+</script>
+
+<template>
+  <dialog id="card_modal" class="modal">
+    <div class="modal-box">
+      <div className="flex flex-col gap-4">
+        <input type="text" placeholder="Title" class="input input-bordered w-full" v-model="title" />
+        <select class="select select-bordered w-full" v-model="type">
+          <option disabled selected>Type</option>
+          <option :value="CardType.note">Note</option>
+          <option :value="CardType.url">URL</option>
+        </select>
+        <textarea className="textarea textarea-bordered text-black h-40" placeholder="Writing anything..." v-model="content" />
+      </div>
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn mr-4">Close</button>
+          <button class="btn btn-primary" @click="createCard(title, type, content)">Add</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+</template>
--- server/routes.ts
+++ server/routes.ts
@@ -1,8 +1,7 @@
 import { ObjectId } from "mongodb";
 
-import { Card, Friend, Mindmap, Post, User, WebSession } from "./app";
-import { CardDoc, CardOptions } from "./concepts/card";
-import { PostDoc, PostOptions } from "./concepts/post";
+import { Card, Mindmap, User, WebSession } from "./app";
+import { CardDoc, CardType } from "./concepts/card";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
 import { Router, getExpressRouter } from "./framework/router";
@@ -55,86 +54,6 @@ class Routes {
   async logOut(session: WebSessionDoc) {
     WebSession.end(session);
     return { msg: "Logged out!" };
-  }
-
-  @Router.get("/posts")
-  async getPosts(author?: string) {
-    let posts;
-    if (author) {
-      const id = (await User.getUserByUsername(author))._id;
-      posts = await Post.getByAuthor(id);
-    } else {
-      posts = await Post.getPosts({});
-    }
-    return Responses.posts(posts);
-  }
-
-  @Router.post("/posts")
-  async createPost(session: WebSessionDoc, content: string, options?: PostOptions) {
-    const user = WebSession.getUser(session);
-    const created = await Post.create(user, content, options);
-    return { msg: created.msg, post: await Responses.post(created.post) };
-  }
-
-  @Router.patch("/posts/:_id")
-  async updatePost(session: WebSessionDoc, _id: ObjectId, update: Partial<PostDoc>) {
-    const user = WebSession.getUser(session);
-    await Post.isAuthor(user, _id);
-    return await Post.update(_id, update);
-  }
-
-  @Router.delete("/posts/:_id")
-  async deletePost(session: WebSessionDoc, _id: ObjectId) {
-    const user = WebSession.getUser(session);
-    await Post.isAuthor(user, _id);
-    return Post.delete(_id);
-  }
-
-  @Router.get("/friends")
-  async getFriends(session: WebSessionDoc) {
-    const user = WebSession.getUser(session);
-    return await User.idsToUsernames(await Friend.getFriends(user));
-  }
-
-  @Router.delete("/friends/:friend")
-  async removeFriend(session: WebSessionDoc, friend: string) {
-    const user = WebSession.getUser(session);
-    const friendId = (await User.getUserByUsername(friend))._id;
-    return await Friend.removeFriend(user, friendId);
-  }
-
-  @Router.get("/friend/requests")
-  async getRequests(session: WebSessionDoc) {
-    const user = WebSession.getUser(session);
-    return await Responses.friendRequests(await Friend.getRequests(user));
-  }
-
-  @Router.post("/friend/requests/:to")
-  async sendFriendRequest(session: WebSessionDoc, to: string) {
-    const user = WebSession.getUser(session);
-    const toId = (await User.getUserByUsername(to))._id;
-    return await Friend.sendRequest(user, toId);
-  }
-
-  @Router.delete("/friend/requests/:to")
-  async removeFriendRequest(session: WebSessionDoc, to: string) {
-    const user = WebSession.getUser(session);
-    const toId = (await User.getUserByUsername(to))._id;
-    return await Friend.removeRequest(user, toId);
-  }
-
-  @Router.put("/friend/accept/:from")
-  async acceptFriendRequest(session: WebSessionDoc, from: string) {
-    const user = WebSession.getUser(session);
-    const fromId = (await User.getUserByUsername(from))._id;
-    return await Friend.acceptRequest(fromId, user);
-  }
-
-  @Router.put("/friend/reject/:from")
-  async rejectFriendRequest(session: WebSessionDoc, from: string) {
-    const user = WebSession.getUser(session);
-    const fromId = (await User.getUserByUsername(from))._id;
-    return await Friend.rejectRequest(fromId, user);
   }
 
   @Router.post("/mindmaps")
@@ -205,9 +124,9 @@ class Routes {
   }
 
   @Router.post("/cards")
-  async createCard(session: WebSessionDoc, content: string, options?: CardOptions) {
+  async createCard(session: WebSessionDoc, title: string, type: CardType, content: string) {
     const user = WebSession.getUser(session);
-    const created = await Card.create(user, content, options);
+    const created = await Card.create(user, type, title, content);
     return { msg: created.msg, card: await Responses.card(created.card) };
   }
 
