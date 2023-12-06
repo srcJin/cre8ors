6dd9a6eb1e4a888aecb0071f0d0ca2b99feb6429
Esther Faith <efk@mit.edu>
2023-12-06 03:13:50 -0500

    awaiting API Key

--- server/concepts/autosuggestion.ts
+++ server/concepts/autosuggestion.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import DocCollection, { BaseDoc } from "../framework/doc";
 
 export interface AutosuggestionOptions {
@@ -13,10 +14,35 @@ export default class AutosuggestionConcept {
   public readonly autosuggestion = new DocCollection<AutosuggestionDoc>("autosuggestion");
 
   async suggest(ideaBlockContent: string[], options?: AutosuggestionOptions) {
-    // loop
-    // gpt call
-    const _id = await this.autosuggestion.createOne({ ideaBlockContent, options });
-    return { msg: "Suggestion created successfully!", autosuggestion: await this.autosuggestion.readOne({ _id }) };
+    const prompt = ideaBlockContent.join("\n");
+
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/engines/davinci/completions",
+        {
+          prompt: prompt,
+          max_tokens: 150, // Adjust as needed
+          ...options, // Any additional options you want to pass
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer YOUR_API_KEY", // TODO replace with our actual API key
+          },
+        },
+      );
+
+      const suggestions = response.data.choices.map((choice: { text: string }) => choice.text.trim());
+      const generatedOrderText = response.data.choices[0].text;
+      const generatedOrder = generatedOrderText.split(",").map((num: string) => parseInt(num.trim()));
+
+      // Reorder the suggestions based on the specified order
+      const orderedSuggestions = generatedOrder.map((index: string | number) => suggestions[index]);
+      return orderedSuggestions;
+    } catch (error) {
+      console.error("Error making GPT-3 API call:", error);
+      throw error;
+    }
   }
 
   async accept() {
